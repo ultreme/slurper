b883c0cc9f9562e1208519685a26f896365cd2f2
Sebastien Rannou <mxs@sbrk.org>
2015-12-18 19:03:18 +0100

    Initial dummy version.

--- cmd/slurper/main.go
+++ cmd/slurper/main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"github.com/ultreme/slurper"
+)
 
 // main is the entrypoint
 func main() {
-	fmt.Printf("Hello world\n")
+	s := slurper.NewSlurper()
+	inputs := os.Args[1:]
+
+	for i := range(inputs) {
+		fmt.Printf(s.Slurp(inputs[i]))
+	}
+
+	if len(inputs) != 0 {
+		fmt.Printf("\n")
+	}
 }
--- slurp.go
+++ slurp.go
@@ -1,1 +1,36 @@
-package slurp
+package slurper
+
+import (
+	"math/rand"
+	"strings"
+)
+
+// Slurper slurps
+type Slurper interface {
+	Slurp(string) string
+}
+
+// Slurp implements the slurper interface
+type Slurp struct {
+}
+
+// Slurp slurps
+func (s *Slurp) Slurp(in string) string {
+	var res string
+
+	words := strings.Split(in, " ")
+	for idx := range(words) {
+		if rand.Int() % 3 == 1 {
+			res += "slurp "
+		}
+		res += words[idx]
+		res += " "
+	}
+
+	return res
+}
+
+// NewSlurper returns a brand new Slurper
+func NewSlurper() Slurper {
+	return &Slurp{}
+}
